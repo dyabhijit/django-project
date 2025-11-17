00433019b9cf0336093539c51fbc7cc281e999c6
Abhijit Das <dyabhijit.das10@gmail.com>
2025-11-17 23:15:01 +0530

    initial django project upload

---
+++ chaiaurdjango/chaiaurdjango/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello, World. You are at chai aur Django home page")
+    return render(request , 'website/index.html')
+
+
+def about(request):
+    # return HttpResponse("Hello, World. You are at chai aur Django about page")
+    return render(request , 'website/about.html')
+
+def contact(request):
+    # return HttpResponse("Hello, World. You are at chai aur Django contact page")
+    return render(request , 'website/contact.html')
